793d9812c8117448758bbee20427fa6865b4aa62
KadryDAV <alialkadryofficial@gmail.com>
2024-12-17 01:58:37 -0800

    changed api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', // Adjust the baseURL as needed
-  withCredentials: true,
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
+    withCredentials: true, 
 });
 
 export default api;
